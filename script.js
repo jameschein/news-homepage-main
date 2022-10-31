bc77ec8fe938ec5e709718ab2f4e2e94c566144a
jamesschein <james@schein.co.uk>
2022-10-31 15:50:47 +0000

    Final Page

--- script.js
+++ script.js
@@ -2,32 +2,35 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     const hamburger = document.querySelector('.hamburger');
     const mainNavbar = document.querySelector('.main-navbar');
+    const hamburgerIcon = document.querySelector('img', hamburger);
 
 
     const toggleMenu = (e) => {
         
         if(hamburger.classList.contains('hamburger-show')){
             hamburger.classList.remove('hamburger-show')
-            mainNavbar.classList.remove('main-navbar-show')
 
+            hamburgerIcon.src = "assets/images/icon-menu.svg"
+         
             // hide the menu
             mainNavbar.style.right = -100 + '%';
             mainNavbar.style.transition = 'right 250ms';
 
         } else {
             hamburger.classList.add('hamburger-show')
-            mainNavbar.classList.add('main-navbar-show')
+
+            // change the icon svg
+            hamburgerIcon.src = "assets/images/icon-menu-close.svg"
             
             // show the menu
             mainNavbar.style.right = 0;
             mainNavbar.style.transition = 'right 250ms';
-            
-
         }
 
         e.preventDefault();
     }
 
+    // set up toggle event
     hamburger.addEventListener('click', toggleMenu)
 
 })
